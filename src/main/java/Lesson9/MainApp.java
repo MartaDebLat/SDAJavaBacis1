22c17e7eaf82cdb7f90c253eaffaa9a8e52ef74f
MartaDebLat <marta.deblat@gmail.com>
2020-01-12 11:20:05 +0100

    Initial commit.

---
+++ src/main/java/Lesson9/MainApp.java
@@ -0,0 +1,13 @@
+package Lesson9;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        UnitConverter.celciusToFahrenhrit(12);
+        UnitConverter.kilometresToMiles(12);
+        UnitConverter.milesTokilometres(12);
+        UnitConverter.kilosToPounds(12);
+        UnitConverter.PoundsToKilos(12);
+
+    }
+}
---
+++ src/main/java/Lesson9/UnitConverter.java
@@ -0,0 +1,70 @@
+package Lesson9;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class UnitConverter {
+
+    public static void celciusToFahrenhrit(double c) {
+        //  double c;    //temp in celcius
+        double fa;     //temp in fahrenhait
+
+
+        fa = (c * 1.8) + 32;
+
+        System.out.print
+                (c + " temp in celcious is in fahrenhait " + fa);
+        System.out.println(" ");
+    }
+
+    public static void kilometresToMiles(double km) {
+        // double km;    // kilometr =
+        double mi;     //mile =
+
+
+        mi = (km * 0.62137);
+
+        System.out.print
+                (km + " kilometres are equalas " + mi + " in miles. ");
+        System.out.println(" ");
+
+    }
+
+    public static void milesTokilometres(double mil) {
+        double km;    // kilometr =
+        //  double mil;     //mile =
+
+        km = (mil / 0.62137);
+
+
+        System.out.print
+                (mil + " miles are equalas " + km + " in kilometres. ");
+        System.out.println(" ");
+
+    }
+
+    public static void kilosToPounds(double kg) {
+        // double kg;    // kilograms
+        double lbs;     //pounds
+
+        lbs = (kg * 2.2046);
+
+        System.out.print
+                (kg + " kilos are equalas " + lbs + " in kilometres. ");
+        System.out.println(" ");
+    }
+
+    public static void PoundsToKilos(double lbs) {
+        double kg;    // kilograms
+        // double lbs;   pounds
+
+        kg = (lbs / 2.2046);
+
+        System.out.print
+                (kg + " kilos are equalas " + lbs + " in kilometres. ");
+        System.out.println(" ");
+    }
+}
+
+
+
