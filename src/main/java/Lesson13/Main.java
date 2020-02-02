85337b44bb017ed10f247c007a346c086666b7dc
MartaDebLat <marta.deblat@gmail.com>
2020-02-02 14:42:42 +0100

    Sort by compare to

--- src/main/java/Lesson13/Main.java
+++ src/main/java/Lesson13/Main.java
@@ -1,11 +1,31 @@
 package Lesson13;
 
 
+import Lesson9_10.Person;
+
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        Person personOne = new Person("John", "Smith", "123", LocalDate.of(1987, 12, 1));
+        Person personTwo = new Person("Ann", "Smith", "324", LocalDate.of(2002, 6, 28));
+        Person personThree = new Person("Zusa", "Args", "444", LocalDate.of(1985, 5, 11));
+        Person personFourth = new Person("Caroline", "Koy", "432", LocalDate.of(1988, 1, 1));
+        Person personFive = new Person("Abdullah", "Ali", "256", LocalDate.of(1958, 1, 12));
+        Person personSix = new Person("Big", "O", "521", LocalDate.of(1984, 1, 15));
+        Person personSeven = new Person("Gertruda", "Shmidt", "332", LocalDate.of(2000, 1, 1));
+        Person personEight = new Person("TEST", "ACTOR", "312", null);
+
+        Person[] people = {personOne, personTwo, personThree, personFourth, personFive, personSix, personSeven};
+        Arrays.sort(people);
+        System.out.println(Arrays.toString(people));
+
+    }
+
+    public static void quickSortTest() {
         int[] arrOne = {12, 5, 3, 5, 6, 3, 353, 42};
         int[] arrTwo = {12, -5, 3, 5, 2332, 13, 353, 0, 42, 55, 6666, 34};
         System.out.println("arrOne");
--- src/main/java/Lesson9_10/Person.java
+++ src/main/java/Lesson9_10/Person.java
@@ -1,16 +1,21 @@
 package Lesson9_10;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalDate;
 
-public class Person {
+public class Person implements Comparable <Person>{
     String firstName, lastName, peselNumber;
     LocalDate yearOfBirth;
 
-    Person(String firstName, String lastName, String peselNumber, LocalDate yearOfBirth) {
+    public Person(String firstName, String lastName, String peselNumber, LocalDate yearOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.peselNumber = peselNumber;
         this.yearOfBirth = yearOfBirth;
+    }
+
+    public Person(Object o) {
     }
 
     public String getFirstName() {
@@ -27,6 +32,22 @@ public class Person {
 
     public LocalDate getYearOfBirth() {
         return yearOfBirth;
+    }
+
+    public int compareTo(Person otherPerson) {
+        return otherPerson.yearOfBirth.compareTo(this.yearOfBirth);
+ //       if (new Person(null))
+ //           return "NO DATA";
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", peselNumber='" + peselNumber + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                '}';
     }
 }
 
