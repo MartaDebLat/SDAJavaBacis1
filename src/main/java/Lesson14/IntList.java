06194fafb402c3d95e2af51360ff457b45968576
MartaDebLat <marta.deblat@gmail.com>
2020-02-08 11:32:24 +0100

    Int as Int / int as Double

---
+++ src/main/java/Lesson14/IntList.java
@@ -0,0 +1,16 @@
+package Lesson14;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntList {
+
+    public static List<Integer> intsToStream() {
+        List<Integer> integerList = Arrays.asList
+                (2, -10, 4, -14, 5, 9, 11, -45, -33,
+                100, 37, 27, 12, 56, -100, -30, 16, 0, 78, 111);
+        return integerList;
+    }
+
+
+}
--- src/main/java/Lesson14/Main.java
+++ src/main/java/Lesson14/Main.java
@@ -3,29 +3,48 @@ package Lesson14;
 import kotlin.collections.UCollectionsKt;
 
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 
 public class Main {
 
+    private static List<Integer> listIntigers;
+
     public static void main(String[] args) {
         List<String>words = WordsProvider.provide();
 
-        List<String> wordsOnLettedD = words.stream()
-        .filter(singleWord -> singleWord.startsWith("d"))
-                .collect(Collectors.toList());
-        System.out.println(wordsOnLettedD);
+//        List<String> wordsOnLettedD = words.stream()
+//        .filter(singleWord -> singleWord.startsWith("d"))
+//                .collect(Collectors.toList());
+//        System.out.println(wordsOnLettedD);
+//
+//       List<String> shortWordsonAwith4letters = words.stream()
+//                .filter( word -> word.startsWith("a")&& word.length()<=4)
+//                .collect(Collectors.toList());
+//
+//        System.out.println(shortWordsonAwith4letters);
+//
+//        List<String> upperCaseWords = words.stream().map(s ->s.substring(0,1).toUpperCase()+s.substring(1))
+//                .collect(Collectors.toList());
+//
+//        System.out.println(upperCaseWords);
 
-       List<String> shortWordsonAwith4letters = words.stream()
-                .filter( word -> word.startsWith("a")&& word.length()<=4)
-                .collect(Collectors.toList());
+        listIntigers = IntList.intsToStream();
 
-        System.out.println(shortWordsonAwith4letters);
+    int sum = listIntigers.stream()
+            .mapToInt(i->i)
+            .sum();
+        System.out.println("Sum of elements is "+ sum);
+        OptionalDouble average = listIntigers.stream()
+                .mapToInt ( i->i)
+                .average();
 
-        List<String> upperCaseWords = words.stream().map(s ->s.substring(0,1).toUpperCase()+s.substring(1))
-                .collect(Collectors.toList());
+        double averageAsDouble = average.getAsDouble() ;
+        System.out.println( "average of elements is: " + averageAsDouble);
 
-        System.out.println(upperCaseWords);
+
 
     }
+
 }
 
